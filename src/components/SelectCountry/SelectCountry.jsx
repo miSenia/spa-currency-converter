625984c9b062f767268339d1c3267f14f4063a15
Ksenia Miroshnikova <k.s.e.n.y.a.miroshnikova1963@gmail.com>
2023-04-28 11:59:44 +0300

    added component for country selection

---
+++ src/components/SelectCountry/SelectCountry.jsx
@@ -0,0 +1,11 @@
+
+const SelectCountry = (props) => {
+  const { value, setValue } = props;
+  // fetch("https://restcountries.com/v3.1/all");
+
+  return (
+    <></>
+  )
+}
+
+export { SelectCountry };
