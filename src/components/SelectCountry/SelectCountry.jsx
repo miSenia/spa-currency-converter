187583604e3a6b755bfc945bebb0e96be0981270
Ksenia Miroshnikova <k.s.e.n.y.a.miroshnikova1963@gmail.com>
2023-05-05 00:41:01 +0300

    updated component for selecting currency

--- src/components/SelectCountry/SelectCountry.jsx
+++ src/components/SelectCountry/SelectCountry.jsx
@@ -1,11 +1,30 @@
-
-const SelectCountry = (props) => {
-  const { value, setValue } = props;
-  // fetch("https://restcountries.com/v3.1/all");
+import { useFetch } from "../../hooks/useFetch";
+import { Dropdown } from "react-bootstrap";
+import { useState } from 'react';
+ 
+const SelectCountry = (currencies, onSelect) => {
+  //const { value, setValue, label } = props;
+  //const [data, loaded, error] = useFetch("https://restcountries.com/v3.1/all");
+  const [selectedCurrency, setSelectedCurrency] = useState('');
+  const handleSelect = (eventKey) => {
+    setSelectedCurrency(eventKey);
+    onSelect(eventKey);
+  }
 
   return (
-    <></>
+    <Dropdown onSelect={handleSelect}>
+      <Dropdown.Toggle variant="secondary" id="dropdown-currency">
+        {selectedCurrency ? selectedCurrency : 'Select Currency'}
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {currencies.map((currency, index) => (
+          <Dropdown.Item key={index} eventKey={currency}>
+            {currency}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
   )
 }
 
-export { SelectCountry };
+export default SelectCountry;
