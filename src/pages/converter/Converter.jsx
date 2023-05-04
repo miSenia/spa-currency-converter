540761723d13103db103dc5e0fa1350738d2a89a
Ksenia Miroshnikova <k.s.e.n.y.a.miroshnikova1963@gmail.com>
2023-05-05 00:41:46 +0300

    changed converter page

--- src/pages/converter/Converter.jsx
+++ src/pages/converter/Converter.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import AmountInput from "../../components/AmountInput/AmountInput";
+import SelectCountry from "../../components/SelectCountry/SelectCountry";
 import { Button, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 
 const Converter = () => {
   return (
-    <Container fluid>
+    <Container style={{ position: "absolute", top: 100, start: 50, backgroundColor: "white" }} >
       <Row>
-        <Col md={5}>
-          <AmountInput></AmountInput>
-          {/* <SelectCountry value={fromCurrency} setValue={setFromCurrency} label="From" /> */}
-          {/* <SwitchCurrency /> */}
-          {/* <SelectCountry value={toCurrency} setValue={setToCurrency} label="To" /> */}
-          <Button variant="primary" type="submit" style={{ width: "50%", border: "none" }}></Button>
+        <Col md={6}><Link to="/">Converter</Link></Col>
+        <Col md={6}><Link to="/currencies">Currencies</Link></Col>
+      </Row>
+      <Row>
+        <AmountInput></AmountInput>
+         <SelectCountry/> {/*value={fromCurrency} setValue={setFromCurrency} label="From" /> */}
+        {/* <SwitchCurrency /> */}
+        {/* <SelectCountry value={toCurrency} setValue={setToCurrency} label="To" /> */}
+      </Row>
+      <Row>
+        <Col md={3} className="output">
+          <></>
+        </Col>
+        <Col md={3} className="button">
+          <Button variant="primary" type="submit" style={{ width: "50%", border: "none" }}>Convert</Button>
         </Col>
       </Row>
     </Container>
