98a9dae66fc6a6f7e11d025302bf7d2ef4df19d3
Ksenia Miroshnikova <k.s.e.n.y.a.miroshnikova1963@gmail.com>
2023-05-16 11:03:15 +0300

    added useAxios props

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,18 +1,26 @@
 import axios from 'axios';
 import { useEffect, useState } from "react";
 
-const useAxios = (url) => {
+const useAxios = (url, apiKey, method = 'GET') => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loaded, setLoaded] = useState(false);
-
+  // var myHeaders = new Headers();
+  // myHeaders.append("apiKey", { apiKey });
   useEffect(() => {
     setLoaded(true);
-    axios(url)
+    axios({
+      url,
+      method,
+      headers: myHeaders,
+      // params: {
+      //   api_key: apiKey,
+      // },
+    })
       .then((res) => setData(res.data))
       .catch((error) => setError(error))
       .finally(setLoaded(false));
-  }, [url]);
+  }, [url, apiKey, method]);
 
   return [data, error, loaded];
 
