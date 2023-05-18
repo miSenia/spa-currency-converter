bf528a8258044d163f73e43f3f3c88f7ed05068d
Ksenia Miroshnikova <k.s.e.n.y.a.miroshnikova1963@gmail.com>
2023-05-18 14:29:49 +0300

    modified converter page

--- src/pages/converter/Converter.jsx
+++ src/pages/converter/Converter.jsx
@@ -2,11 +2,9 @@ import React, { useContext, useState } from "react";
 import AmountInput from "../../components/AmountInput/AmountInput";
 import SelectCountry from "../../components/SelectCountry/SelectCountry";
 import SwitchCurrency from "../../components/SwitchCurrency/SwitchCurrency";
+import LinkBar from "../../components/LinksBar/LinkBar";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import { NavLink, Link, useLocation } from "react-router-dom";
 import { CurrencyContext } from "../../context/CurrencyContext";
-import cn from 'classnames';
-import styles from './Converter.module.scss';
 
 
 const Converter = () => {
@@ -18,40 +16,35 @@ const Converter = () => {
     firstAmount,
   } = useContext(CurrencyContext);
   const [result, setResult] = useState(0);
-  const location = useLocation();
-  
+
   return (
-    <Container style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", backgroundColor: "white", width: "50%", padding: "2rem", boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1)", borderRadius: "1rem" }} >
-      <Row className="links-container" style={{textAlign:"center"}}>
-        <Col md={6}>
-          {/* <NavLink to="/" className={styles.page_link} activeClassName={styles.page_link__active}><i class="bi bi-currency-exchange" style={{ padding: "4px" }}></i>Converter</NavLink>  */}
-           <Link to="/" className={cn(styles.page_link, { [styles.page_link__active]: location.pathname === '/' })}><i class="bi bi-currency-exchange" style={{ padding: "4px" }}></i>Converter</Link>
-        </Col>
-        <Col md={6}>
-           {/* <NavLink to="/currencies" className={styles.page_link} activeClassName={styles.page_link__active}><i class="bi bi-graph-up" style={{ padding: "4px" }}></i>Currencies</NavLink> */}
-          <Link to="/currencies" className={cn(styles.page_link,  {[styles.page_link__active]: location.pathname === '/currencies' })}><i class="bi bi-graph-up" style={{ padding: "4px" }}></i>Currencies</Link>
-        </Col>
+    <>
+    <h2>Currency Converter</h2>
+    <Container style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", backgroundColor: "white", width: "50%", boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1)", borderRadius: "1rem", padding: "0" }} >
+      <LinkBar></LinkBar>
+      <Row className="converter-container" style={{ padding: "2rem", margin: "0" }}>
+        <Row className="input-container" style={{ paddingBottom: "2rem" }}>
+          <Col md={6}><AmountInput></AmountInput></Col>
+          <Col><SelectCountry value={fromCurrency} setValue={setFromCurrency} /></Col>
+          <Col><SwitchCurrency /></Col>
+          <Col><SelectCountry value={toCurrency} setValue={setToCurrency} /></Col>
+        </Row>
+        <Row className="output-container" >
+          <Col md={9}>
+            <Card style={{ border: "none" }}>
+              <Card.Body>
+                <Card.Title style={{ color: "grey" }}>Converted Amount</Card.Title>
+                <Card.Text></Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={3} className="button">
+            <Button variant="primary" type="submit" style={{ width: "100%", border: "none" }}>Convert</Button>
+          </Col>
+        </Row>
       </Row>
-      <Row className="converter-container" style={{ paddingTop: "2rem", paddingBottom: "2rem" }}>
-        <Col md={5}><AmountInput></AmountInput></Col>
-        <Col><SelectCountry value={fromCurrency} setValue={setFromCurrency} /></Col>
-        <Col><SwitchCurrency /></Col>
-        <Col><SelectCountry value={toCurrency} setValue={setToCurrency} /></Col>
-      </Row>
-      <Row className="output-container" >
-        <Col md={9}>
-          <Card style={{ border: "none" }}>
-            <Card.Body>
-              <Card.Title style={{ color: "grey" }}>Converted Amount</Card.Title>
-              <Card.Text></Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={3} className="button">
-          <Button variant="primary" type="submit" style={{ width: "100%", border: "none" }}>Convert</Button>
-        </Col>
-      </Row>
-    </Container>
+      </Container>
+      </>
   );
 };
 
