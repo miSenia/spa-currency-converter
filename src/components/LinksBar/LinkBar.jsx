a2a0011d2e69e1fc45a92093d214b556d09d79d5
Ksenia Miroshnikova <k.s.e.n.y.a.miroshnikova1963@gmail.com>
2023-07-20 11:52:53 +0300

    linkbar updates

--- src/components/LinksBar/LinkBar.jsx
+++ src/components/LinksBar/LinkBar.jsx
@@ -8,17 +8,36 @@ const LinkBar = () => {
 
   const location = useLocation();
 
+  function classForLink(pathname) {
+    return cn(
+      styles.page_link,
+      (location.pathname === pathname) && styles.page_link__active,
+    )
+  }
+
+  // TODO: избавится от inline-стилей
   return (
-    <Row className="links-container" style={{ textAlign: "center", borderBottom: "1px", borderBottomColor: "grey", paddingTop: "2rem" }}>
-      <Col md={6}>
-        <Link to="/" className={cn(styles.page_link, { [styles.page_link__active]: location.pathname === '/' })}><i class="bi bi-currency-exchange" style={{ padding: "4px" }}></i>Converter</Link>
+    <Row className="links-container" style={{
+      textAlign: "center",
+      borderBottom: "1px",
+      borderBottomColor: "grey",
+      paddingTop: "2rem",
+      paddingBottom: "2rem"
+    }}>
+      <Col className="col-converter-link" md={6}>
+        <Link to="/" className={classForLink('/')}>
+          <i class="bi bi-currency-exchange" style={{ padding: "4px" }}></i>
+          Converter
+        </Link>
       </Col>
-      <Col md={6}>
-        <Link to="/currencies" className={cn(styles.page_link, { [styles.page_link__active]: location.pathname === '/currencies' })}><i class="bi bi-graph-up" style={{ padding: "4px" }}></i>Currencies</Link>
+      <Col className="col-currencies-link" md={6}>
+        <Link to="/currencies" className={classForLink('/currencies')}>
+          <i class="bi bi-graph-up" style={{ padding: "4px" }}></i>
+          Currencies
+        </Link>
       </Col>
     </Row>
   );
-};
-
+}
 
 export default LinkBar;
